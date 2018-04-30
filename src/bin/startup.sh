8bccaa1c1ba3670362dd18442e2dbbdc98550d16
Rik Brugman <rik@rikbrugman.nl>
2018-04-30 10:42:52 +0200

    Minor fixes

--- src/bin/startup.sh
+++ src/bin/startup.sh
@@ -8,7 +8,7 @@
 xinput set-prop 15 "libinput Tapping Enabled" 1
 
 # VGA/Graphics
-xrandr --output LVDS-1 --primary --auto --output HDMI-1-1 --auto --left-of LVDS-1
+xrandr --output LVDS-1 --primary --auto --output HDMI-1-1 --auto --right-of LVDS-1
 feh --bg-scale 'pictures/city.jpg'
 
 # Keyboard
