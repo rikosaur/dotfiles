8f13fa403fc4ab35b0a051abb7457733d28fa139
Rik Brugman <rik@rikbrugman.nl>
2018-05-10 13:13:06 +0200

    Fix for localhost

--- src/desktop-rik/bin/startup.sh
+++ src/desktop-rik/bin/startup.sh
@@ -1,1 +1,20 @@
-/home/rik/dotfiles/src/bin/startup.sh
+#!/bin/sh
+
+# Startup Commands
+# Rik Brugman 2017
+
+# VGA/Graphics
+xrandr --output HDMI-3 --primary --auto --output HDMI-4 --auto --right-of HDMI-3
+feh --bg-scale 'pictures/city.jpg'
+
+# Keyboard
+xbindkeys &
+
+# append home to path
+PATH=$PATH:$HOME/bin
+
+# Others
+bin/start-polybar
+dunst &
+dropbox &
+google-drive-ocamlfuse /home/rik/google-drive &
