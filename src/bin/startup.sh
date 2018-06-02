85af8f56252fb1eadfd2e7991c92c8f730711715
Rik Brugman <rik@rikbrugman.nl>
2018-06-02 19:44:30 +0200

    Restored Tap-On-Touch

--- src/bin/startup.sh
+++ src/bin/startup.sh
@@ -3,6 +3,9 @@
 # Startup Commands
 # Rik Brugman 2017
 
+# Enable Tap-On-Touch
+xinput set-prop 15 "libinput Tapping Enabled" 1
+
 # VGA/Graphics
 xrandr --output LVDS-1 --primary --auto --output HDMI-1-1 --auto --right-of LVDS-1
 feh --bg-scale 'pictures/city.jpg'
