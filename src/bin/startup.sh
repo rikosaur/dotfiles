bede28e5a91322ff8773f025d0d240837e3f4da8
Rik Brugman <rik@rikbrugman.nl>
2018-06-18 09:34:00 +0200

    Some more fixes...

--- src/bin/startup.sh
+++ src/bin/startup.sh
@@ -8,7 +8,7 @@ xinput set-prop 15 "libinput Tapping Enabled" 1
 
 # VGA/Graphics
 xrandr --output LVDS-1 --primary --auto --output HDMI-1-1 --auto --right-of LVDS-1
-feh --bg-scale 'pictures/city.jpg'
+feh --bg-scale 'pictures/background.jpg'
 
 # Keyboard
 xbindkeys &
