ce99381006c9f14794685fe26565ca59ed72ee83
Rik Brugman <rik@rikbrugman.nl>
2019-08-13 20:52:04 +0200

    Add fish config

---
+++ src/.config/fish/config.fish
@@ -0,0 +1,12 @@
+# setup aliases
+alias ll='exa -lah --git'
+alias ll2='exa -lah --git -TL=2'
+alias llg='ll | grep --color=always'
+alias ll2g='ll2 | grep --color=always'
+alias ls='exa'
+alias gc='git commit'
+alias gd='git diff'
+alias ga='git add'
+alias gs='git status'
+alias bd='bin/console'
+alias doei='shutdown now'
